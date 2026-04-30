8c6260062ef2538ccdff34b475bdc0776e9f80bf
Argo Workflows <argo@devops.local>
2026-04-30 20:56:46 +0000

    feat: terraform apply and ansible init K3s cluster new-k3s

---
+++ envs/shared/new-k3s/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc07"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://192.168.1.35:8006/api2/json"
+  pm_api_token_id     = "terraform@pam!terraform"
+  pm_api_token_secret = "bdcbd933-8cbf-4c6c-be85-41b0b8f74ba3"
+  pm_tls_insecure     = true
+  pm_parallel         = 4
+}
+
+module "k3s_cluster" {
+  source        = "../../../modules/k3s-cluster"
+  cluster_name  = "new-k3s"
+  proxmox_nodes = ["machine-1", "machine-2"]
+  template_name = "debian-k3s"
+  master_count  = 1
+  worker_count  = 1
+  cores         = 4
+  memory        = 4096
+  disk_size     = 20
+}
